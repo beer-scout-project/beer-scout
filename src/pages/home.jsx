0c93dfa1353c34f8f4d5f13fe442323580290d3c
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-07-31 15:03:29 -0230

    Testing permissions

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const home = () => {
-  return <div className='bg-red-500'>home</div>;
+	return <div className='bg-red-500'>Find the cheapest beer near you!</div>;
 };
 
 export default home;

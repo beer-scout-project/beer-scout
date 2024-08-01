eacb1cb7726da9f6110054b7e0b4e495c10819a2
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-08-01 19:48:09 -0230

    fix text

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ const home = () => {
   return (
     <div className='flex justify-center mt-10'>
       <p className='bg-primary text-lg font-bold p-2 rounded-lg'>
-        Welcome to the home page!
+        Find the cheapest beer near you!
       </p>
     </div>
   );

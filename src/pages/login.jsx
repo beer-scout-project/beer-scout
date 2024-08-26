beb2313aafcb9bb6e3ccca40990bf7d4caa706af
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-08-26 14:48:27 -0230

    Update login page inputs
    Add email and password fields for user to login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const login = () => {
-  return (
-    <div className='p-10'>
-      <p>Login page</p>
-      <div className='divider'>OR</div>
-      <div className='flex items-center space-x-2'>
-        <p>Don't have an account?</p>
-        <Link to='/signup' className='link'>
-          Sign Up
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div className='p-10'>
+			<p>Login page</p>
+			<div className='container'>
+				<form>
+					<label htmlFor='email' className='block'>
+						<span className='block'>Email</span>
+						<input type='email' name='email'></input>
+					</label>
+					<label htmlFor='password' className='block'>
+						<span className='block'>Password</span>
+						<input type='password' name='password'></input>
+					</label>
+				</form>
+				<button className='block btn btn-primary'>Login</button>
+				<Link>
+					<span className='block link'>Forgot Password?</span>
+				</Link>
+			</div>
+			<div className='divider'>OR</div>
+			<div className='flex items-center space-x-2'>
+				<p>Don't have an account?</p>
+				<Link to='/signup' className='link'>
+					Sign Up
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default login;

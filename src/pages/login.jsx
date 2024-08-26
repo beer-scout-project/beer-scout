ed46fb87fe90be3abb45dafdc5cd24c203b296c6
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-08-26 16:13:22 -0230

    Update login page layout
    Update the layout of the page for a better UI/UX, and to make inputs wider for larger screens.

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,17 +3,24 @@ import { Link } from 'react-router-dom';
 
 const login = () => {
 	return (
-		<div className='p-10'>
-			<p>Login page</p>
-			<div className='container'>
+		<div className='p-10 flex flex-col items-center gap-8 m-auto'>
+			<div className='flex flex-col gap-6 w-full max-w-[325px]'>
 				<form>
 					<label htmlFor='email' className='block'>
 						<span className='block'>Email</span>
-						<input type='email' name='email'></input>
+						<input
+							type='email'
+							name='email'
+							className='p-1 rounded w-full'
+						></input>
 					</label>
 					<label htmlFor='password' className='block'>
 						<span className='block'>Password</span>
-						<input type='password' name='password'></input>
+						<input
+							type='password'
+							name='password'
+							className='p-1 rounded w-full'
+						></input>
 					</label>
 				</form>
 				<button className='block btn btn-primary'>Login</button>

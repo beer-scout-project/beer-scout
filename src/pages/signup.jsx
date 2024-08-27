8148b48dcd01da7802102bf732c3d369516258a0
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-08-27 15:48:51 -0230

    Create CreateAccountForm component
    Create a component for the user create an account form for reusability and simplifying the sign up page.

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,81 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CreateAccountForm from "../components/CreateAccountForm";
 
-const signup = () => {
+export default function SignUp() {
   return (
     <div className="flex flex-col items-center gap-8 p-10">
-      <h2 className="self-start">Create your account</h2>
-      <div className="flex w-full max-w-[325px] flex-col">
-        <form className="flex flex-col gap-4">
-          <div>
-            <label htmlFor="first-name" className="block">
-              First Name
-            </label>
-            <input
-              type="text"
-              name="first-name"
-              className="w-full rounded p-1 text-black"
-            ></input>
-          </div>
-          <div>
-            <label htmlFor="last-name" className="block">
-              Last Name
-            </label>
-            <input
-              type="text"
-              name="last-name"
-              className="w-full rounded p-1 text-black"
-            ></input>
-          </div>
-          <div>
-            <label htmlFor="display-name" className="block">
-              Display Name*
-            </label>
-            <input
-              type="text"
-              name="display-name"
-              className="w-full rounded p-1 text-black"
-            ></input>
-          </div>
-          <div>
-            <label htmlFor="email" className="block">
-              Email*
-            </label>
-            <input
-              type="email"
-              name="email"
-              className="laceholder-slate-400focus:outline-none w-full rounded p-1 text-black shadow-sm invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:ring-1 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none"
-            ></input>
-          </div>
-          <div>
-            <label htmlFor="password" className="block">
-              Password*
-            </label>
-            <input
-              type="password"
-              name="password"
-              className="w-full rounded p-1 text-black"
-            ></input>
-          </div>
-          <div>
-            <label htmlFor="confirm-password" className="block">
-              Confirm Password*
-            </label>
-            <input
-              type="password"
-              name="confirm-password"
-              className="w-full rounded p-1 text-black"
-            ></input>
-          </div>
-          <button
-            type="submit"
-            onClick={() => console.log("register")}
-            className="btn btn-primary mt-4 block"
-          >
-            Register
-          </button>
-        </form>
-      </div>
+      <CreateAccountForm />
       <div className="divider">OR</div>
       <div className="flex w-full max-w-[325px] flex-col items-center gap-2">
         <p>Already have an account?</p>
@@ -85,6 +15,4 @@ const signup = () => {
       </div>
     </div>
   );
-};
-
-export default signup;
+}

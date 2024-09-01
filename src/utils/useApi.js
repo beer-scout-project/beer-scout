fe073e63f4741a2f89464e4d8fb77a02de96f036
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-09-01 18:15:53 -0230

    Update useApi on mount
    Set user to the user stored in localStorage when page loads if there's a stored user.

--- src/utils/useApi.js
+++ src/utils/useApi.js
@@ -4,11 +4,20 @@ import User from "../utils/User";
 export default function useApi() {
   const [user, setUser] = useState(null);
 
+  // Store user in local storage
   useEffect(() => {
     if (user) {
       localStorage.setItem("user", JSON.stringify(user));
     }
   }, [user]);
+
+  // Set user from local storage on mount
+  useEffect(() => {
+    const storedUser = getStoredUser();
+    if (storedUser) {
+      setUser(storedUser);
+    }
+  }, []);
 
   const createNewUser = (firstName, lastName, userName, email, password) => {
     const newUser = new User(firstName, lastName, userName, email, password);

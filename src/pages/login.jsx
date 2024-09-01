849c801b9a473556493a988db3e9964a413c1c89
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-09-01 17:47:28 -0230

    Remove label for attribute
    Remove the for attribute to avoid browser warning since it's not required when the input is nested inside the label.

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,7 +6,7 @@ const login = () => {
     <div className="flex flex-col items-center gap-8 p-10">
       <div className="flex w-full max-w-[325px] flex-col gap-6">
         <form className="flex flex-col gap-4">
-          <label htmlFor="email" className="block">
+          <label className="block">
             <span className="block">Email</span>
             <input
               type="email"
@@ -14,7 +14,7 @@ const login = () => {
               className="placeholder-slate-400focus:outline-none w-full rounded p-1 text-black shadow-sm invalid:border-pink-500 invalid:text-pink-600 focus:border-sky-500 focus:ring-1 focus:ring-sky-500 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:border-slate-200 disabled:bg-slate-50 disabled:text-slate-500 disabled:shadow-none"
             ></input>
           </label>
-          <label htmlFor="password" className="block">
+          <label className="block">
             <span className="block">Password</span>
             <input
               type="password"

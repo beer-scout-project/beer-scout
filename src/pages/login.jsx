5a3cac02f3640813e8ccfe10f34a2b6fcc3104c8
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-09-02 15:29:01 -0230

    Update login validation alerts
    Display notification banner with corresponding validation message.

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,22 +5,79 @@ import useApi from "../utils/useApi";
 export default function Login() {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
+  const [userValidation, setUserValidation] = useState(null);
   const { validateUser } = useApi();
 
   const handleLogin = (e) => {
     e.preventDefault();
+    // check if user info is correct
     const isUserValid = validateUser(userEmail, userPassword);
+    // if user is valid, set user validation to valid
     if (isUserValid === "valid") {
-      alert("Login successful");
+      setUserValidation("valid");
     } else if (isUserValid === "invalid") {
-      alert("Login failed");
+      setUserValidation("invalid");
     } else if (isUserValid === "user does not exist") {
-      alert("User does not exist");
+      setUserValidation("User does not exist");
     }
   };
 
   return (
     <div className="flex flex-col items-center gap-8 p-10">
+      {userValidation === "valid" && (
+        <div role="alert" className="alert alert-success">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>You have logged in successfully!</span>
+        </div>
+      )}
+      {userValidation === "invalid" && (
+        <div role="alert" className="alert alert-warning">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
+          </svg>
+          <span>Invalid email address or password</span>
+        </div>
+      )}
+      {userValidation === "user does not exist" && (
+        <div role="alert" className="alert alert-error">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span>Please create an account first</span>
+        </div>
+      )}
       <div className="flex w-full max-w-[325px] flex-col gap-6">
         <form className="flex flex-col gap-4">
           <label className="block">

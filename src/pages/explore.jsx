46ca495f87170c9c31ed04aa08bf768fbd38ec99
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-09-09 16:27:01 -0230

    Add beverage posts to explore page
    Add test beverage info posts to the explore page.
    Add temporary images for location avatar.

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import BeverageInfoPost from "../components/BeverageInfoPost";
 
 export default function Explore() {
   return (
@@ -8,6 +8,20 @@ export default function Explore() {
       </div>
       <div>
         <h6>Happening Now</h6>
+        <div className="flex flex-col gap-4">
+          <BeverageInfoPost
+            locationImage="/public/temp-images/bannerman-logo.jpg"
+            locationName="Bannerman Brewing Co."
+            beverageInfo="All pints are $5 each"
+            additionalInfo="From 4pm to 6pm"
+          />
+          <BeverageInfoPost
+            locationImage="/public/temp-images/brewdock-logo.png"
+            locationName="Brewdock"
+            beverageInfo="Jugs for $25"
+            additionalInfo="Anything from our rotating tape selection #1 or #2"
+          />
+        </div>
       </div>
       <div>
         <h6>Recent posts</h6>

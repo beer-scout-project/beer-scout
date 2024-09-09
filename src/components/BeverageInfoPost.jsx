fd8ea45ec051afc126112dd577a793dcd90ba62a
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-09-09 16:55:32 -0230

    Add user posts to explore page
    Add temporary user posts to the explore page.
    Adds temporary user images.

--- src/components/BeverageInfoPost.jsx
+++ src/components/BeverageInfoPost.jsx
@@ -3,20 +3,20 @@ import PropTypes from "prop-types";
 export default function BeverageInfoPost(props) {
   return (
     <div>
-      <div className="card min-w-max max-w-96 bg-base-200 shadow-xl">
+      <div className="card w-full bg-base-200 shadow-xl">
         <div className="card-body p-6">
           <div className="flex items-center gap-2">
             <div className="avatar">
               <div className="h-8 w-8 rounded-full">
-                <img src={props.locationImage} />
+                <img src={props.avatarImage} />
               </div>
             </div>
             <div>
-              <p className="font-bold">{props.locationName}</p>
+              <p className="font-bold">{props.avatarName}</p>
             </div>
           </div>
           <div>
-            <p className="text-secondary">{props.beverageInfo}</p>
+            <p className="text-secondary">{props.primaryInfo}</p>
             <p className="font-light">{props.additionalInfo}</p>
           </div>
         </div>
@@ -26,8 +26,8 @@ export default function BeverageInfoPost(props) {
 }
 
 BeverageInfoPost.propTypes = {
-  locationImage: PropTypes.string.isRequired,
-  locationName: PropTypes.string.isRequired,
-  beverageInfo: PropTypes.string.isRequired,
+  avatarImage: PropTypes.string.isRequired,
+  avatarName: PropTypes.string.isRequired,
+  primaryInfo: PropTypes.string.isRequired,
   additionalInfo: PropTypes.string,
 };
--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -10,21 +10,35 @@ export default function Explore() {
         <h6>Happening Now</h6>
         <div className="flex flex-col gap-4">
           <BeverageInfoPost
-            locationImage="/public/temp-images/bannerman-logo.jpg"
-            locationName="Bannerman Brewing Co."
-            beverageInfo="All pints are $5 each"
+            avatarImage="/public/temp-images/bannerman-logo.jpg"
+            avatarName="Bannerman Brewing Co."
+            primaryInfo="All pints are $5 each"
             additionalInfo="From 4pm to 6pm"
           />
           <BeverageInfoPost
-            locationImage="/public/temp-images/brewdock-logo.png"
-            locationName="Brewdock"
-            beverageInfo="Jugs for $25"
+            avatarImage="/public/temp-images/brewdock-logo.png"
+            avatarName="Brewdock"
+            primaryInfo="Jugs for $25"
             additionalInfo="Anything from our rotating tape selection #1 or #2"
           />
         </div>
       </div>
       <div>
         <h6>Recent posts</h6>
+        <div className="flex flex-col gap-4">
+          <BeverageInfoPost
+            avatarImage="/public/temp-images/girl1-avatar.jpg"
+            avatarName="Jane Smith"
+            primaryInfo="Bannerman Brewing Co."
+            additionalInfo="Had their All Hands and it was crisp and light."
+          />
+          <BeverageInfoPost
+            avatarImage="/public/temp-images/guy1-avatar.jpg"
+            avatarName="Frank Douglas"
+            primaryInfo="Brewdock"
+            additionalInfo="Had a jug of Port Rexton Gluten Free Lager, and I would highly recommend!"
+          />
+        </div>
       </div>
       <div>
         <h6>All Happy Hours</h6>

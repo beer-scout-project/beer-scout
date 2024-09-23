aff1b5258ccc5975cfec6537c61146aca599e09e
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-09-23 14:21:01 -0230

    Add add bar price form info
    Add information about the "Add a New Bar Price Form" for users to help them when submitting a bar price.

--- src/pages/aboutPage.jsx
+++ src/pages/aboutPage.jsx
@@ -7,13 +7,15 @@ export default function AboutPage() {
       }}
     >
       <div className="hero-overlay bg-black bg-opacity-60 p-4 md:p-16">
-        <div className="rounded-lg bg-[#FDEBD0] bg-opacity-85 p-4 text-[#2f2f2f] md:p-8">
-          <div className="mb-12">
-            <h4 className="mb-4">About</h4>
+        <div className="flex flex-col gap-12 rounded-lg bg-[#FDEBD0] bg-opacity-85 p-4 text-[#2f2f2f] md:p-8">
+          <div className="flex flex-col gap-4">
+            <h4>About</h4>
             <p>
-              Beer Scout shows you the locations that have the cheapest beer
-              prices near you (currently only available for Downtown St.
-              John’s).
+              Beer Scout is here to help you track down the best beer deals in
+              Downtown St. John's! 🍻 We want you to hang out with your friends
+              and family without stressing about your wallet. So, let us know
+              when you spot a great price—together, we can keep Beer Scout
+              growing and our community thriving! Cheers! 🍻✨
             </p>
           </div>
           <div className="flex flex-col gap-8 md:flex-row">
@@ -40,6 +42,35 @@ export default function AboutPage() {
                   location name. You can submit a price correction if you find a
                   price has changed.
                 </li>
+              </ol>
+            </div>
+          </div>
+          <div className="flex flex-col gap-8 md:flex-row">
+            <img
+              className="max-w-[275px] rounded-lg"
+              src="/add-bar-form-happy-hr-350x906.png"
+              alt="form for use to submit a beer price"
+            />
+            <div className="flex flex-col flex-wrap gap-4">
+              <h5>Add a New Bar Price Form</h5>
+              <p>
+                Have a new location or beer price update that you'd like to
+                share? Fill out this form to send us your suggested update.
+              </p>
+              <ol className="list-decimal pl-5">
+                <li>Enter the name of the bar or location.</li>
+                <li>Select the city that the bar is located in.</li>
+                <li>Choose the serving size (in milliliters).</li>
+                <li>Enter the price of the beer.</li>
+              </ol>
+              <p>
+                Happy Hour details are optional. If you have a happy hour, check
+                the tickbox and complete these details.
+              </p>
+              <ol className="list-decimal pl-5">
+                <li>Select the day of the week that the happy hour is on.</li>
+                <li>Enter the start time of the happy hour.</li>
+                <li>Enter the end time of the happy hour.</li>
               </ol>
             </div>
           </div>

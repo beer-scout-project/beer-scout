d650bafbf01624803261475954bbb2ab3d3eda6f
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2024-10-15 15:55:26 -0230

    new main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,12 @@ console.log("Main file loaded");
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  createBrowserRouter,
+} from "react-router-dom";
 import "./index.css";
 import Nav from "./components/nav.jsx";
 import Home from "./pages/home.jsx";
@@ -12,31 +17,45 @@ import AddBarForm from "./pages/addBarForm.jsx";
 import TempDisplay from "./pages/tempDisplay.jsx";
 import AboutPage from "./pages/aboutPage.jsx";
 
-const App = () => (
-  <Router basename='/beer-scout/'>
-    <div className="app-container">
-      <Nav />
-      <div className="content-container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/bar-list" element={<BarList />} />
-          <Route path="/add-bar" element={<AddBarForm />} />
-          {/* temp */}
-          <Route path="/temp" element={<TempDisplay />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </div>
-    </div>
-  </Router>
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ],
+  { basename: "/beer-scout" },
 );
 
-const container = document.getElementById("root");
-if (!container.__reactRoot) {
-  container.__reactRoot = ReactDOM.createRoot(container);
-}
-
-container.__reactRoot.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
+
+// const App = () => (
+//   <Router basename='/beer-scout/'>
+//     <div className="app-container">
+//       <Nav />
+//       <div className="content-container">
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/bar-list" element={<BarList />} />
+//           <Route path="/add-bar" element={<AddBarForm />} />
+//           {/* temp */}
+//           <Route path="/temp" element={<TempDisplay />} />
+//           <Route path="/about" element={<AboutPage />} />
+//         </Routes>
+//       </div>
+//     </div>
+//   </Router>
+// );a
+// const container = document.getElementById("root");
+// if (!container.__reactRoot) {
+//   container.__reactRoot = ReactDOM.createRoot(container);
+// }
+// container.__reactRoot.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );

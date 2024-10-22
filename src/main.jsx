6e3b19d36e69f150d02fa649f2897213df271a4b
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-10-22 17:47:56 -0230

    Add Google analytics
    Add Google analytics to track how user interact with the website and how many user visit the app.

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 console.log("Main file loaded");
 
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import "./index.css";
 import Nav from "./components/nav.jsx";
 import Home from "./pages/home.jsx";
@@ -10,21 +15,41 @@ import BarList from "./pages/barList.jsx";
 import AddBarForm from "./pages/addBarForm.jsx";
 import AboutPage from "./pages/aboutPage.jsx";
 
-const App = () => (
-  <Router>
-    <div className="app-container">
-      <Nav />
-      <div className="content-container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/bar-list" element={<BarList />} />
-          <Route path="/add-bar" element={<AddBarForm />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
+const usePageTracking = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag("config", "G-GGWTJ6V32P", {
+        page_path: location.pathname,
+      });
+    }
+  }, [location]);
+};
+
+const PageTrackingWrapper = () => {
+  usePageTracking();
+  return null; // Render nothing
+};
+
+const App = () => {
+  return (
+    <Router>
+      <PageTrackingWrapper />
+      <div className="app-container">
+        <Nav />
+        <div className="content-container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/bar-list" element={<BarList />} />
+            <Route path="/add-bar" element={<AddBarForm />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  </Router>
-);
+    </Router>
+  );
+};
 
 const container = document.getElementById("root");
 if (!container.__reactRoot) {

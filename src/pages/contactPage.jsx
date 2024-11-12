36a7e0b08404dd171d0b01e80f60f19b1822069c
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-11-12 13:41:23 -0330

    Update contactPage.jsx

--- src/pages/contactPage.jsx
+++ src/pages/contactPage.jsx
@@ -14,8 +14,8 @@ export default function ContactPage() {
       const result = await emailjs.sendForm(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-        e.target,
         import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+        e.target,
       );
 
       if (result.status === 200) {
